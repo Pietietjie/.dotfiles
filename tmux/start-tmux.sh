2cf47b701faa65113b3327b61b999a824174f70b
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-01-05 08:06:28 +0200

    👨‍💻: made a tmux startup script

---
+++ tmux/start-tmux.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+session="dot"
+directory=~/.dotfiles
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="82"
+directory=~/proj/laradock8.2
+tmux send-keys -t $session:1 'dc up -d nginx phpmyadmin'
+tmux new-session -d -s $session -c $directory
+
+session="74"
+directory=~/proj/laradock7.4
+tmux send-keys -t $session:1 'dc up -d nginx phpmyadmin'
+tmux new-session -d -s $session -c $directory
+
+session="pay"
+directory=~/proj/bfi-pay-platform
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="foodshare"
+directory=~/proj/foodshare
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="monify"
+directory=~/proj/monify
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="better-bond"
+directory=~/proj/monify-better-bond
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="blu"
+directory=~/proj/monify-blue-deals
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+
+session="westfalia"
+directory=~/proj/westfalia-frontend
+tmux new-session -d -s $session -c $directory
+tmux send-keys -t $session:1 'nv' C-m
+tmux split-window -c $directory -t $session:1 -v -p 15
+tmux new-window -t $session:3 -c $directory
+tmux send-keys -t $session:3 'npm run dev' C-m
+directory=~/proj/westfalia-backend
+tmux new-window -t $session:2 -c $directory
+tmux send-keys -t $session:2 'nv' C-m
+tmux split-window -c $directory -t $session:2 -v -p 15
+
+tmux attach-session -t "dot"
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -92,5 +92,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+tmux has-session 2>/dev/null
+
+if [ $? != 0 ]; then
+    zsh "$dot/tmux/start-tmux.sh"
+fi
+
 # update_dot="g dotfiles pl"
 # "${update_dot}" &>/dev/null & disown;
