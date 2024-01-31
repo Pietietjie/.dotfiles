76ff30492f5848d4ab7ca2c6ac1801cf85aeff56
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-01-31 08:48:14 +0200

    👨‍💻 added telecope git bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -466,6 +466,10 @@ end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sts', require('telescope.builtin').git_status, { desc = '[S]earch by gi[T] [S]tatus' })
+vim.keymap.set('n', '<leader>stc', require('telescope.builtin').git_commits, { desc = '[S]earch by gi[T] [C]ommits' })
+vim.keymap.set('n', '<leader>sth', require('telescope.builtin').git_stash, { desc = '[S]earch by gi[T] stas[H]' })
+vim.keymap.set('n', '<leader>stb', require('telescope.builtin').git_branches, { desc = '[S]earch by gi[T] [B]ranch' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
