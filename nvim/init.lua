dedb3839cd3aff5f2587756a4f40d64828349ae2
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-06-20 09:27:04 +0200

    ✨ added neovim binding for opening netrw in the project root

--- nvim/init.lua
+++ nvim/init.lua
@@ -596,6 +596,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>-', function () vim.cmd('Explore .') end, { desc = 'Open Netrw in the project root'})
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
