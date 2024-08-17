63ccdcfdf71815796ff93e7c5336f90bbd9d79bb
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-08-17 10:43:48 +0200

    🚸 improved the tmux start command to have the vim panes selected and the first windows selected

--- tmux/start-tmux.sh
+++ tmux/start-tmux.sh
@@ -5,7 +5,8 @@ directory=~/.dotfiles
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
-tmux resize-pane -Z $session:1 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 
 session="82"
 directory=~/proj/laradock8.2
@@ -22,40 +23,47 @@ directory=~/proj/bfi-pay-platform
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
-tmux resize-pane -Z $session:1 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 
 session="foodshare"
 directory=~/proj/foodshare
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
-tmux resize-pane -Z $session:1 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 
 session="monify"
 directory=~/proj/monify
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
-tmux resize-pane -Z $session:1 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 
 session="better-bond"
 directory=~/proj/monify-better-bond
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 tmux new-window -t $session:4 -c $directory
 tmux send-keys -t $session:4 'npm run dev' C-m
 directory=~/proj/monify-loan-cards
 tmux new-window -t $session:2 -c $directory
 tmux send-keys -t $session:2 'nv' C-m
 tmux split-window -c $directory -t $session:2 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 directory=~/proj/monify-better-bond/vendor/lavalamplab/monifyloanapi
 tmux new-window -t $session:3 -c $directory
 tmux send-keys -t $session:3 'nv' C-m
 tmux split-window -c $directory -t $session:3 -v -p 15
-tmux resize-pane -Z $session:1 
-tmux resize-pane -Z $session:2 
-tmux resize-pane -Z $session:3 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
+tmux select-window -t 1
 
 session="ooba"
 directory=~/proj/monify-ooba
@@ -64,51 +72,61 @@ tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
 tmux new-window -t $session:4 -c $directory
 tmux send-keys -t $session:4 'npm run dev' C-m
-tmux resize-pane -Z $session:1 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 directory=~/proj/monify-loan-cards
 tmux new-window -t $session:2 -c $directory
 tmux send-keys -t $session:2 'nv' C-m
 tmux split-window -c $directory -t $session:2 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 directory=~/proj/monify-ooba/vendor/lavalamplab/monifyloanapi
 tmux new-window -t $session:3 -c $directory
 tmux send-keys -t $session:3 'nv' C-m
 tmux split-window -c $directory -t $session:3 -v -p 15
-tmux resize-pane -Z $session:1 
-tmux resize-pane -Z $session:2 
-tmux resize-pane -Z $session:3 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
+tmux select-window -t 1
 
 session="blu"
 directory=~/proj/monify-blue-deals
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 tmux new-window -t $session:4 -c $directory
 tmux send-keys -t $session:4 'npm run dev' C-m
 directory=~/proj/monify-loan-cards
 tmux new-window -t $session:2 -c $directory
 tmux send-keys -t $session:2 'nv' C-m
 tmux split-window -c $directory -t $session:2 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 directory=~/proj/monify-blue-deals/vendor/lavalamplab/monifyloanapi
 tmux new-window -t $session:3 -c $directory
 tmux send-keys -t $session:3 'nv' C-m
 tmux split-window -c $directory -t $session:3 -v -p 15
-tmux resize-pane -Z $session:1 
-tmux resize-pane -Z $session:2 
-tmux resize-pane -Z $session:3 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
+tmux select-window -t 1
 
 session="westfalia"
 directory=~/proj/westfalia-frontend
 tmux new-session -d -s $session -c $directory
 tmux send-keys -t $session:1 'nv' C-m
 tmux split-window -c $directory -t $session:1 -v -p 15
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
 tmux new-window -t $session:3 -c $directory
 tmux send-keys -t $session:3 'npm run dev' C-m
 directory=~/proj/westfalia-backend
 tmux new-window -t $session:2 -c $directory
 tmux send-keys -t $session:2 'nv' C-m
 tmux split-window -c $directory -t $session:2 -v -p 15
-tmux resize-pane -Z $session:1 
-tmux resize-pane -Z $session:2 
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
+tmux select-window -t 1
 
 tmux attach-session -t "dot"
 
