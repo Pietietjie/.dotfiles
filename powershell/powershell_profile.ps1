dbe47ab7a58691773a443f8a75d6a7fb2922814a
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-08-20 21:12:38 +0200

    🔧 added powershellrc

---
+++ powershell/powershell_profile.ps1
@@ -0,0 +1,13 @@
+Set-Alias -Name g -Value git
+# 1 Character Aliases
+set-alias -Name a -Value artisan
+set-alias -Name d -Value docker
+set-alias -Name g -Value git
+set-alias -Name p -Value php
+set-alias -Name q -Value exit
+# Remaining Aliases
+set-alias -Name :q -Value exit
+set-alias -Name so -Value refreshenv
+set-alias -Name vim -Value nvim
+set-alias -Name cl -Value clear
+
