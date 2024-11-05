5d5e88f66c486330808a78da2b278114d3a8d2a3
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-11-05 12:18:26 +0200

    🪟🔧 updated the powershell config to so that all aliases are working and that it is also applied to powershell 7

--- powershell/powershell_profile.ps1
+++ powershell/powershell_profile.ps1
@@ -4,10 +4,19 @@ set-alias -Name a -Value artisan
 set-alias -Name d -Value docker
 set-alias -Name g -Value git
 set-alias -Name p -Value php
-set-alias -Name q -Value exit
 # Remaining Aliases
-set-alias -Name :q -Value exit
 set-alias -Name so -Value refreshenv
-set-alias -Name vim -Value nvim
+remove-item -Force Alias:nv
+set-alias -Name nv -Value nvim
 set-alias -Name cl -Value clear
 
+function q {
+    Invoke-command -ScriptBlock {exit}
+}
+function :q {
+    Invoke-command -ScriptBlock {exit}
+}
+## allows the use of choco utilities
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+## adds bash like unix commands instead of the useless windows ones
+Set-PSReadLineOption -EditMode Emacs
