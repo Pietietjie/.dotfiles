9668d227bf2c58f25ce849cd564d9e0793930448
Pieter <pieter.vandermeijden@lavalamp.biz>
2024-11-05 13:18:26 +0200

    💄 cusomized prompt for powershell 🪟

--- powershell/powershell_profile.ps1
+++ powershell/powershell_profile.ps1
@@ -20,3 +20,45 @@ function :q {
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 ## adds bash like unix commands instead of the useless windows ones
 Set-PSReadLineOption -EditMode Emacs
+
+function prompt {
+    # #Assign Windows Title Text
+    # $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    $Date = Get-Date -Format ' HH:mm:ss'
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    if (Get-Error) {
+        Write-Host "󰻌  " -ForegroundColor Red -NoNewline
+    } else {
+        Write-Host "󰳉  " -ForegroundColor Green -NoNewline
+    }
+    if ($IsAdmin) {
+        Write-host '🪟   ' -ForegroundColor Red -NoNewline
+    } else { 
+        Write-host '🪟  ' -ForegroundColor Blue -NoNewline
+    }
+    Write-Host "$($CmdPromptUser.Name.split("\")[1])@$($CmdPromptUser.Name.split("\")[0])" -ForegroundColor Blue -NoNewline
+    Write-Host ":"  -ForegroundColor Yellow -NoNewline
+    Write-Host "$pwd "  -ForegroundColor Green -NoNewline
+
+    Write-Host "$date" -ForegroundColor Red
+    if (Get-Error) {
+        Write-Host "╰─" -NoNewline -ForegroundColor Red
+    } else {
+        Write-Host "╰─" -NoNewline -ForegroundColor Green
+    }
+    Write-Host " " -NoNewline -ForegroundColor Magenta
+    if (git branch) {
+        Write-Host "  $(git rev-parse --abbrev-ref HEAD) " -NoNewline -ForegroundColor Magenta
+        if (git status) {
+            Write-Host "  " -NoNewline -ForegroundColor Yellow
+        }
+    } else {
+        Write-Host "   " -NoNewline -ForegroundColor Red
+    }
+    Write-Host ":" -NoNewline -ForegroundColor Magenta
+    return " "
+} #end prompt function
