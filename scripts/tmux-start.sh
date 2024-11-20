4dca397261cd7fbdfe3fd50c4ad82b4a7820d0ab
Pieter <21083972+Pietietjie@users.noreply.github.com>
2024-11-20 11:23:02 +0200

    🧑‍💻✨ made that the tm start command can spesify in the json if it should use nvim and if any additional commands need be run on start up 🎨 made that the proj session is defined in the json

--- scripts/tmux-start.sh
+++ scripts/tmux-start.sh
@@ -3,36 +3,36 @@
 if ! (tmux ls 2>/dev/null | sed 's/:.*//' | grep -q 'dot'); then
     session="dot";
     directory=~/.dotfiles;
-    tmux new-session -d -s $session -c $directory;
+    tmux new-session -d -s $session -c "$directory";
     tmux send-keys -t $session:1 'nv' C-m;
-    tmux split-window -c $directory -t $session:1 -v -p 15;
+    tmux split-window -c "$directory" -t $session:1 -v -p 15;
     tmux select-pane -t $session:1;
     tmux resize-pane -Z -t $session:1;
 else
     echo "the Dotfiles session all ready exists";
 fi
 
-if ! (tmux ls 2>/dev/null | sed 's/:.*//' | grep -q 'proj'); then
-    session="proj";
-    directory=~/proj/;
-    tmux new-session -d -s $session -c $directory;
-    tmux send-keys -t $session:1 "powershell.exe -Command \"Start-Process 'C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe'\" && dc up" C-m;
-else
-    echo "the Project session all ready exists";
-fi
-
-if [ -f ~/tmux-sessions.json ]; then 
-    selectedSessions="$(jq -r '.[].sessionName' ~/tmux-sessions.json | fzf -m)"
+if [ -f ~/tmux-sessions.json ]; then
+    selectedSessions="$(jq -r '.[].sessionName' ~/tmux-sessions.json | fzf -m --tac)"
     jq -c '.[]' ~/tmux-sessions.json | while IFS= read -r sessionJson; do
         sessionName=$(echo "$sessionJson" | jq -r '.sessionName')
         if echo $selectedSessions | grep -q $sessionName; then
-            directory=$(echo "$sessionJson" | jq -r '.dir')
+            directory=$(echo "$sessionJson" | jq -r '.dir' | sed "s|^~|$HOME|")
+            nvim=$(echo "$sessionJson" | jq -r '.nvim')
+            extraCommands=$(echo "$sessionJson" | jq -r '.extraCommands')
             if ! (tmux ls 2>/dev/null | sed 's/:.*//' | grep -q "$sessionName"); then
-                tmux new-session -d -s $sessionName -c $directory;
-                tmux send-keys -t $sessionName:1 'nv' C-m;
-                tmux split-window -c $directory -t $sessionName:1 -v -p 15;
-                tmux select-pane -t $sessionName:1;
-                tmux resize-pane -Z -t $sessionName:1;
+                tmux new-session -d -s $sessionName -c "$directory";
+                availableWindow=1
+                if [[ -n $nvim && "$nvim" != "null" ]]; then
+                    availableWindow=2
+                    tmux send-keys -t $sessionName:1 'nv' C-m;
+                    tmux split-window -c "$directory" -t $sessionName:1 -v -p 15;
+                    tmux select-pane -t $sessionName:1;
+                    tmux resize-pane -Z -t $sessionName:1;
+                fi
+                if [[ -n $extraCommands && "$extraCommands" != "null" ]]; then
+                    tmux send-keys -t $sessionName:$avialableWindow "$extraCommands" C-m;
+                fi
             else
                 echo "the $sessionName session all ready exists";
             fi
