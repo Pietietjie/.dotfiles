72cad00dbe359fe7082002c787ca874a4a7aeb17
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-02-26 14:01:07 +0200

    🔧 changed the config of the snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -871,7 +871,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-e>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
@@ -880,7 +880,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-a>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.locally_jumpable(-1) then
