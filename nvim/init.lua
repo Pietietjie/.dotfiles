2116f8a6f1271cbeb068029ead18313569d74e1f
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-03-05 08:14:53 +0200

    🔥 removed code that was used for stupid tab navigation behavior

--- nvim/init.lua
+++ nvim/init.lua
@@ -878,18 +878,14 @@ cmp.setup {
       select = true,
     },
     ['<C-e>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
     ['<C-a>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
