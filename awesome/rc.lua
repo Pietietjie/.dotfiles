65a5e43077fffb08e27a4f8763f86716a9b8901c
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-05-20 21:40:05 +0200

    ✨ added a color picker for arch

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -269,6 +269,13 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey, }, "Return", function() awful.spawn(terminal) end,
         { description = "open a terminal", group = "launcher" }),
+    awful.key(
+        { modkey, "Shift" }, "c",
+        function()
+            awful.spawn.with_shell("gpick -ops | sed 's/\\n//g' | xsel -ib")
+        end,
+        { description = "pick color to clipboard (no newline)", group = "utilities" }
+    ),
     awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
     awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
     awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
