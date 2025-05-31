6afb07d4677a8d50dc310961a2a951cf0a9de65d
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-05-31 18:56:36 +0200

    🔧 made the wez term config \w the intention of not using it with tmux

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -25,42 +25,134 @@ config.animation_fps = 1
 config.disable_default_key_bindings = true
 config.default_cursor_style = 'SteadyBar'
 
--- timeout_milliseconds defaults to 1000 and can be omitted
-config.leader = { key = '\'', mods = 'CTRL' }
+config.leader = { key = ' ', mods = 'CTRL' }
+local is_zoomed = false
+local function zoom_toggle(window, pane)
+  is_zoomed = not is_zoomed
+  window:perform_action(act.SetPaneZoomState(is_zoomed), pane)
+end
+local function navigate_panes_with_zoom(window, pane, direction)
+  window:perform_action(act.ActivatePaneDirection(direction), pane)
+  if is_zoomed then
+    window:perform_action(act.SetPaneZoomState(true), pane)
+  end
+end
+
 config.keys = {
-  -- Send "CTRL-'" to the terminal when pressing CTRL-', CTRL-'
-  { key = "'", mods = 'LEADER|CTRL',  action=wezterm.action{SendKey={key='a',mods='CTRL'}}},
-  { key = "\"",mods = "LEADER|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  { key = "%", mods = "LEADER|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
-  { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-  { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-  { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-  { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-  { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-  { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-  { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-  { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-  { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-  { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-  { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-  { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-  { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-  { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-  { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-  { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-  { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-  { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-  { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-  { key = '=', mods = 'CTRL',         action = act.IncreaseFontSize },
-  { key = '-', mods = 'CTRL',         action = act.DecreaseFontSize },
-  { key = '0', mods = 'CTRL',         action = act.ResetFontSize },
-  { key = 'C', mods = 'SHIFT|CTRL',   action = act.CopyTo 'Clipboard' },
-  { key = 'v', mods = 'CTRL',         action = act.PasteFrom 'Clipboard' },
+  -- Send CTRL space to the terminal when pressing CTRL space, CTRL space
+  {
+    key = " ",
+    mods = 'LEADER|CTRL',
+    action = wezterm.action { SendKey = { key = ' ', mods = 'CTRL' } },
+  },
+  {
+    key = '"',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action {
+      SplitVertical = { domain = 'CurrentPaneDomain' },
+    },
+  },
+  {
+    key = '%',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action {
+      SplitHorizontal = { domain = 'CurrentPaneDomain' },
+    },
+  },
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action {
+      SplitVertical = { domain = 'CurrentPaneDomain' },
+    },
+  },
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action {
+      SplitHorizontal = { domain = 'CurrentPaneDomain' },
+    },
+  },
+  {
+     key = 'z', mods = 'LEADER',
+    action = wezterm.action_callback(function(window, pane)
+      zoom_toggle(window, pane)
+    end),
+  },
+  {
+    key = 'c',
+    mods = 'LEADER',
+    action = wezterm.action { SpawnTab = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'L',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowDebugOverlay
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(window, pane)
+      navigate_panes_with_zoom(window, pane, 'Left')
+    end),
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(window, pane)
+      navigate_panes_with_zoom(window, pane, 'Down')
+    end),
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(window, pane)
+      navigate_panes_with_zoom(window, pane, 'Up')
+    end),
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(window, pane)
+      navigate_panes_with_zoom(window, pane, 'Right')
+    end),
+  },
+  {
+    key = 'H',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action { AdjustPaneSize = { 'Left', 5 } },
+  },
+  {
+    key = 'J',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action { AdjustPaneSize = { 'Down', 5 } },
+  },
+  {
+    key = 'K',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action { AdjustPaneSize = { 'Up', 5 } },
+  },
+  {
+    key = 'L',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action { AdjustPaneSize = { 'Right', 5 } },
+  },
+  { key = '1', mods = 'LEADER',     action = wezterm.action { ActivateTab = 0 } },
+  { key = '2', mods = 'LEADER',     action = wezterm.action { ActivateTab = 1 } },
+  { key = '3', mods = 'LEADER',     action = wezterm.action { ActivateTab = 2 } },
+  { key = '4', mods = 'LEADER',     action = wezterm.action { ActivateTab = 3 } },
+  { key = '5', mods = 'LEADER',     action = wezterm.action { ActivateTab = 4 } },
+  { key = '6', mods = 'LEADER',     action = wezterm.action { ActivateTab = 5 } },
+  { key = '7', mods = 'LEADER',     action = wezterm.action { ActivateTab = 6 } },
+  { key = '8', mods = 'LEADER',     action = wezterm.action { ActivateTab = 7 } },
+  { key = '9', mods = 'LEADER',     action = wezterm.action { ActivateTab = 8 } },
+  { key = '=', mods = 'CTRL',       action = act.IncreaseFontSize },
+  { key = '-', mods = 'CTRL',       action = act.DecreaseFontSize },
+  { key = '0', mods = 'CTRL',       action = act.ResetFontSize },
+  { key = 'C', mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
+  { key = 'v', mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
 }
+
 
 -- and finally, return the configuration to wezterm
 return config
-
