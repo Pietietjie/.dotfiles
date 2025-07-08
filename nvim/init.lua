264496ff7b00f88694f1bfc0b64eb9f999b71f8c
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-07-08 13:52:31 +0200

    ➕ added nvim noice

--- nvim/init.lua
+++ nvim/init.lua
@@ -391,7 +391,34 @@ require('lazy').setup({
     'eandrju/cellular-automaton.nvim',
     event = "VeryLazy",
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      -- TODO enable
+      -- "rcarriga/nvim-notify",
+    }
+  },
 }, {})
+
+require("noice").setup({
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+})
 
 -- ----------------------------------------------------
 -- colorscheme
