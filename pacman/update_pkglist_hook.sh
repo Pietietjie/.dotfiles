980d5730baf9743bbbd2cda54e44656e208d8068
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-07-09 19:46:26 +0200

    ✨👽 made that the packages output ignores packages that is defined by unix_desktop

---
+++ pacman/update_pkglist_hook.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+OUTPUT_FILE="/etc/pkglist.txt"
+IGNORE_FILE_DESKTOP="/etc/pkglist_desktop.txt"
+IGNORE_FILE_IGNORE="/etc/ignore.txt"
+
+PACKAGE_LIST=$(pacman -Qqet)
+TEMP_IGNORED_PACKAGES=$(mktemp)
+
+if [ -f "$IGNORE_FILE_DESKTOP" ]; then
+    cat "$IGNORE_FILE_DESKTOP" >>"$TEMP_IGNORED_PACKAGES"
+fi
+
+if [ -f "$IGNORE_FILE_IGNORE" ]; then
+    cat "$IGNORE_FILE_IGNORE" >>"$TEMP_IGNORED_PACKAGES"
+fi
+
+cat "$TEMP_IGNORED_PACKAGES"
+
+echo "$PACKAGE_LIST" | grep -vFf "$TEMP_IGNORED_PACKAGES" >"$OUTPUT_FILE"
+
+rm "$TEMP_IGNORED_PACKAGES"
