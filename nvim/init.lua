ef2fcab448b70e3550fd607c114f39bfe396fa25
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-09-05 11:11:27 +0200

    🚸 made that the noice popup does is a bit to the top

--- nvim/init.lua
+++ nvim/init.lua
@@ -414,12 +414,42 @@ require('lazy').setup({
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
     dependencies = {
       "MunifTanjim/nui.nvim",
-      -- TODO enable
-      -- "rcarriga/nvim-notify",
-    }
+    },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "45%",
+            col = "50%",
+          },
+        },
+
+        cmdline_popupmenu = {
+          position = {
+            row = "45%",
+            col = "50%",
+          },
+        },
+      },
+    },
+    keys = {
+      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
+      { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
+      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
+      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
+    },
   },
 
   {
@@ -430,26 +460,6 @@ require('lazy').setup({
     config = true,
   },
 }, {})
-
--- [[ configure noice ]]
-require("noice").setup({
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
-  presets = {
-    bottom_search = true,         -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
-  },
-})
-
-
 
 -- ----------------------------------------------------
 -- colorscheme
@@ -527,7 +537,6 @@ vim.opt.signcolumn = 'yes:1'
 vim.cmd('autocmd User TelescopePreviewerLoaded setlocal number')
 vim.cmd('autocmd FileType netrw setl bufhidden=delete')
 vim.cmd('let g:netrw_fastbrowse = 0')
-vim.cmd('set lazyredraw')
 vim.cmd('let g:netrw_bufsettings = \'noma nomod nu nowrap ro nobl\'')
 vim.cmd('autocmd FileType netrw setlocal number')
 
