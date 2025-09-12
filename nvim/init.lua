9d350563398fc2ee5870cdccd9abbfa5dcd360d4
Pieter <21083972+Pietietjie@users.noreply.github.com>
2025-09-12 10:35:48 +0200

    💄 fixed the undo tree look

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,9 +99,46 @@ require('lazy').setup({
   {
     'mbbill/undotree',
     event = "VeryLazy",
+    config = function()
+      vim.g.undotree_WindowLayout = 2
+      vim.g.undotree_SplitWidth = 35
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.g.undotree_ShortIndicators = 1
+      vim.g.undotree_TreeNodeShape = "󱤆"
+      vim.g.undotree_TreeVertShape = "│"
+      vim.g.undotree_TreeSplitShape = "╱"
+      vim.g.undotree_TreeReturnShape = "╲"
+      vim.g.undotree_DiffCommand = "diff"
+      vim.g.undotree_RelativeTimestamp = 1
+      vim.g.undotree_HighlightChangedText = 1
+      vim.g.undotree_HighlightChangedWithSign = 1
+      vim.g.undotree_HighlightSyntaxAdd = "DiffAdd"
+      vim.g.undotree_HighlightSyntaxChange = "DiffChange"
+      vim.g.undotree_HighlightSyntaxDel = "DiffDelete"
+      vim.g.undotree_HelpLine = 0
+      vim.g.undotree_CursorLine = 1
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "undotree",
+        callback = function()
+          vim.wo.number = false
+          vim.wo.relativenumber = false
+          vim.wo.signcolumn = "no"
+          vim.wo.foldcolumn = "0"
+          vim.wo.statuscolumn = ""
+        end,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "diff",
+        callback = function()
+          vim.wo.wrap = false
+          vim.wo.number = false
+          vim.wo.relativenumber = false
+        end,
+      })
+    end,
   },
 
-  {'sheerun/vim-polyglot'},
+  { 'sheerun/vim-polyglot' },
 
   -- Git related plugins
   'tpope/vim-fugitive',
