89b1cfaca4c9a17bad15fb4753dff817b5b09ac4
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-06 19:07:04 +0200

    ➕ added packages & made that the experimental features are enabled

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 { config, pkgs, ... }:
 
@@ -69,7 +69,11 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Set zsh as default shell for all users.
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.pietietjie = {
     isNormalUser = true;
     description = "Pieter Louis van der Meijden";
@@ -89,8 +93,15 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    keepassxc
+    python3
+    tmux
+    alacritty
+    git
+    neovim
+    fzf
+    ripgrep
+    nodejs_20
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -114,10 +125,11 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
