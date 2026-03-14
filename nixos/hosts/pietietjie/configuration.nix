83d2984ef1959a63c83695d61636c3f3fdd3668f
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-14 10:40:09 +0200

    📦 added niri & rofi

--- nixos/hosts/pietietjie/configuration.nix
+++ nixos/hosts/pietietjie/configuration.nix
@@ -47,13 +47,14 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "za";
-    variant = "";
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
   };
+  programs.niri.enable = true;
+  programs.xwayland.enable = true;
+  xdg.portal.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -173,6 +174,7 @@ fonts = {
     unzip
 
     keepassxc
+    rofi
     alacritty
     kitty
     mpv
