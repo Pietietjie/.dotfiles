69c25d03dcc745ae656feb64aacdef6e6b1cb104
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-19 21:13:12 +0200

    ✨ made that the home manager symlinks out side of the nix store to the dotfiles repo

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,8 +1,13 @@
-{ pkgs, inputs, ... }:
-{
-    home.username = "pietietjie";
-    home.homeDirectory = "/home/pietietjie";
-    home.stateVersion = "25.11";
+{ pkgs, inputs, config, ... }:
+let
+    username = "pietietjie";
+    homeDirectory = "/home/pietietjie";
+    dotfilesPath = "${homeDirectory}/dotfiles";
+    stateVersion = "25.11";
+in {
+    home.username = username;
+    home.homeDirectory = homeDirectory;
+    home.stateVersion = stateVersion;
 
     programs.home-manager.enable = true;
 
@@ -14,67 +19,62 @@
         "$HOME/.local/bin"
     ];
 
-    home.file.".path" = {
-        source = ../shell/.path;
-        force = true;
-    };
-
     home.file.".bashrc" = {
-        source = ../bash/.bashrc;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/bash/.bashrc";
         force = true;
     };
 
     home.file.".gitconfig" = {
-        source = ../git/.gitconfig;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/git/.gitconfig";
         force = true;
     };
 
     home.file.".tmux.conf" = {
-        source = ../tmux/.tmux.conf;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/tmux/.tmux.conf";
         force = true;
     };
 
     home.file.".zshrc" = {
-        source = ../zsh/.zshrc;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zsh/.zshrc";
         force = true;
     };
 
     home.file.".oh-my-zsh/themes/pietietjie.zsh-theme" = {
-        source = ../zsh/pietietjie.zsh-theme;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zsh/pietietjie.zsh-theme";
         force = true;
     };
 
     home.file.".oh-my-zsh/custom/themes/pietietjie.zsh-theme" = {
-        source = ../zsh/pietietjie.zsh-theme;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zsh/pietietjie.zsh-theme";
         force = true;
     };
 
     home.file.".local/share/noctalia" = {
-        source = ../noctalia;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/noctalia";
         recursive = true;
         force = true;
     };
 
     home.file.".local/bin/scrp" = {
-        source = ../zsh/scrp;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zsh/scrp";
         executable = true;
         force = true;
     };
 
     xdg.configFile."alacritty" = {
-        source = ../alacritty;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/alacritty";
         recursive = true;
         force = true;
     };
 
     xdg.configFile."nvim" = {
-        source = ../nvim;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nvim";
         recursive = true;
         force = true;
     };
 
     xdg.configFile."niri/config.kdl" = {
-        source = ../niri/config.kdl;
+        source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/niri/config.kdl";
         force = true;
     };
 }
