0d2db8d58ed1297ddbc4500acda404534ba19c9e
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-19 20:06:21 +0200

    🗑️

--- nvim/init.lua
+++ nvim/init.lua
@@ -1281,7 +1281,7 @@ vim.keymap.set({ 'o', 'x' }, 'ah', require('gitsigns').select_hunk)
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
