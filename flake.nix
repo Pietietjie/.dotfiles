72c95e6bdc5de5465cd4ff7d1f9bccc8fdf189e0
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-21 10:49:35 +0200

    🔥💡removed comments and disable ➖ removed thunar will see if I can get everything done with yazi

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "NixOS Flake Configuration";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; # Or use nixos-24.11 etc.
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
         home-manager = {
             url = "github:nix-community/home-manager/release-25.11";
