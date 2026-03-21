808493fca7fccab82935995ada9afe36c346f3e8
Pieter <21083972+Pietietjie@users.noreply.github.com>
2026-03-21 10:45:56 +0200

    🔥 removed KDE plasma

--- nixos/hosts/pietietjie/configuration.nix
+++ nixos/hosts/pietietjie/configuration.nix
@@ -46,8 +46,6 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
